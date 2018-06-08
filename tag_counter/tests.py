b2a1fe6fd4d6a8b90f7cb2cd5b2ea92d02d3fea0
egormin <neoman2014@gmail.com>
2018-06-08 17:02:29 +0300

    Update tests.py

--- tag_counter/tests.py
+++ tag_counter/tests.py
@@ -4,3 +4,10 @@ from tag_counter.core import get_message
 class HelloworldTestCase(TestCase):
     def test_helloworld(self):
         self.assertEqual(get_message(), 'Hello World!')
+        
+    def test_helloworld1(self):
+        self.assertEqual(get_message(), 'Hello World!')
+        
+    def test_helloworld2(self):
+        self.assertEqual(get_message(), 'Hello World!')
+
